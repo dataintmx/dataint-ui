15a48515ffc72256aa68582532309b8cae065be5
tomiplevak <tplevak@dataint.mx>
2026-01-09 11:38:38 -0300

    Eslint config updated to include css validations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,29 @@
 import js from "@eslint/js";
 import ts from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import css from "@eslint/css";
+import { tailwind4 } from "tailwind-csstree";
 
 export default defineConfig(
+  // Global ignores
   {
     ignores: ["dist/", "node_modules/", "playground/"],
   },
 
-  js.configs.recommended,
-  ...ts.configs.recommendedTypeChecked,
+  // JavaScript linting
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    ...js.configs.recommended,
+  },
+
+  // TypeScript linting
+  ...ts.configs.recommendedTypeChecked.map((config) => ({
+    ...config,
+    files: ["**/*.{ts,tsx}"],
+  })),
 
   {
-    files: ["**/*.ts", "**/*.tsx"],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       parserOptions: {
         projectService: true,
@@ -31,6 +43,21 @@ export default defineConfig(
       ],
 
       "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+
+  // CSS linting
+  {
+    files: ["**/*.css"],
+    language: "css/css",
+    plugins: { css },
+    extends: ["css/recommended"],
+    languageOptions: {
+      customSyntax: tailwind4,
+    },
+    rules: {
+      "css/no-duplicate-imports": "error",
+      "css/no-empty-blocks": "error",
     },
   }
 );
