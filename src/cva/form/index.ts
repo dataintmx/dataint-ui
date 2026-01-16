be5e35f106d228d861aa187483a82b776cc249c9
tomiplevak <tplevak@dataint.mx>
2026-01-16 13:54:54 -0300

    New Form Components CVA added to root exportation

--- src/cva/form/index.ts
+++ src/cva/form/index.ts
@@ -1,2 +1,5 @@
 export * from "@/types/cva/form/checkbox";
 export * from "@/types/cva/form/input";
+export * from "@/types/cva/form/radio";
+export * from "@/types/cva/form/select";
+export * from "@/types/cva/form/toggle";
